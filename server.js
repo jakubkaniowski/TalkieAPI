7b99531e9e4d49a2861656719006ec609a57ce7a
Jakub Kaniowski <jkaniowski@openskydata.com>
2019-11-02 17:45:31 +0100

    Changed port listening for application

--- server.js
+++ server.js
@@ -13,6 +13,4 @@ app.use(express.json());
 const usersRoute = require('./routes/users');
 app.use('/users', usersRoute);
 
-app.listen(3000, () => {
-    console.log("Server is running");
-});
+app.listen(process.env.PORT || 3000);
