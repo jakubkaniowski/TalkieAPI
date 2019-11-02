b0a4954b83a5b89810a247898d00b497d4061677
Jakub Kaniowski <jkaniowski@openskydata.com>
2019-11-02 18:37:39 +0100

    Changes for cors, added cors module to package.json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/talkie', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
@@ -9,6 +10,7 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log("DB connected"));
 
 app.use(express.json());
+app.use(cors());
 
 const usersRoute = require('./routes/users');
 app.use('/users', usersRoute);
